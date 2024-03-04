96f0195c7d70df43ef9497c800c3237ea800f7ec
nordengt <nordenghisingt123@gmail.com>
2024-03-04 20:40:48 +0545

    vgggggggggggggggggg

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # VGG-Implementation
+
+# Configuration
+
+![VGG Configurations](./images/configurations.png)
+
 "Very Deep Convolutional Networks for Large-Scale Image Recognition" by Karen Simonyan and Andrew Zisserman.
+
+Paper: https://arxiv.org/pdf/1409.1556.pdf
+
+# Architecture
+
+![VGG Architecture](./images/architecture.png)
+
+## Different day, same old story
+
+GPU POOR !!!
+
+Didn't train cause I don't have a powerful GPU. But the architecture is there for playing.
+
+## Info
+
+Run script below to checkout the model informations
+
+```sh
+python info.py
+```
+
+### VGG11 (A)
+
+![vgg11](./images/vgg11.jpg)
+
+### VGG11_LRN (A-LRN)
+
+![vgg11_lrn](./images/vgg11_lrn.jpg)
+
+### VGG13 (B)
+
+![vgg13](./images/vgg13.jpg)
+
+### VGG16_1 (C)
+![vgg16_1](./images/vgg16_1.jpg)
+
+### VGG16 (D)
+
+![vgg16](./images/vgg16.jpg)
+
+### VGG19 (D)
+![vgg19](./images/vgg19.jpg)
+
+## Usage
+
+Before running the script, place your data directory location for both train and test data in `root_dir="{DIR}"` here at [dataloader.py](./dataloader/dataloader.py)
+
+```sh
+python train.py --epochs 74 --vgg vgg16
+```
--- model/model.py
+++ model/model.py
@@ -3,12 +3,13 @@ from typing import List, Union
 import torch
 from torch import nn
 
-from vgg_config import cfgs
+from .vgg_config import cfgs
 
 class VGG(nn.Module):
     def __init__(self, vgg_name: str, num_classes: int = 1000) -> None:
         super().__init__()
         self.features = self._make_layers(cfgs[vgg_name])
+        self.avg_pool = nn.AdaptiveAvgPool2d((7, 7))
         self.classifier = nn.Sequential(
             nn.Linear(512*7*7, 4096),
             nn.ReLU(),
@@ -24,20 +25,20 @@ class VGG(nn.Module):
     def _make_layers(self, cfg: List[Union[str, int]]):
         layers = []
         in_channels = 3
-        for x in cfg:
-            if x == "M":
+        for out_channels in cfg:
+            if out_channels == "M":
                 layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
-            elif x == "LRN":
+            elif out_channels == "LRN":
                 layers.append(nn.LocalResponseNorm(size=5, alpha=1e-4, beta=0.75, k=2))
             else:
-                if x == "C1-256" or x == "C1-512":
-                    x = int(x.split("-")[1])
-                    layers.append(nn.Conv2d(in_channels, x, kernel_size=1, stride=1, padding=1))
+                if out_channels == "C1-256" or out_channels == "C1-512":
+                    out_channels = int(out_channels.split("-")[1])
+                    layers.append(nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=1))
                 else:
-                    x = int(x)
-                    layers.append(nn.Conv2d(in_channels, x, kernel_size=3, stride=1, padding=1))
+                    out_channels = int(out_channels)
+                    layers.append(nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1))
                 layers.append(nn.ReLU(inplace=True))
-                in_channels = x
+                in_channels = out_channels
         return nn.Sequential(*layers)
 
     def _init_layers(self):
@@ -48,6 +49,7 @@ class VGG(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.features(x)
+        x = self.avg_pool(x)
         x = torch.flatten(x, 1)
         x = self.classifier(x)
         return x
